430f4407bf383f90ee669bad0e8ce64d8af53beb
errandt <errandt@gmail.com>
2022-08-20 18:30:51 +0900

    C sturdy

--- PCcafe2.c
+++ PCcafe2.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int N, K, i, num = 0;
-	int arr[1000] = { 0 };
+	int N, K, i, num = 1;
+	int arr[4000] = { 0 };
 
 	scanf_s("%d %d", &N, &K);
 	if (N > 10000 || N < 1 || K < 1 || K > N)
@@ -12,7 +12,7 @@ int main()
 		return 0;
 	}
 	
-	for (i = 0; i < N; i++)
+	for (i = 1; i <= N; i++)
 	{
 		if (N % i == 0)
 		{
@@ -21,13 +21,11 @@ int main()
 		}
 	}
 
-	for(i=0;i<num;i++)
-		printf("%d\n", arr[num]);
-
-	//if (K < num)
-	//	printf("%d", arr[K]);
-	//else
-	//	printf("0");
+	
+	if (K < num)
+		printf("%d", arr[K]);
+	else
+		printf("0");
 
 	return 0;
 }
